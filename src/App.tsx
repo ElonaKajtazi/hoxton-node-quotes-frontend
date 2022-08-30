4b071e92f0748312a2bab6b6ec974b929cdc3b72
Elona Kajtazi <MY_NAME@example.com>
2022-08-30 17:37:14 +0200

    getting the quotes from the server

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
+import "./App.css";
+import { useEffect, useState } from "react";
 
-import logo from './logo.svg'
-import './App.css'
 
 function App() {
-
-
+  const [quotes, setQuotes] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/quotes")
+      .then((rsp) => rsp.json())
+      .then((qutesFromServer) => setQuotes(qutesFromServer));
+  }, []);
   return (
     <div className="App">
- <h1>HELLO</h1>
+      <h1>Famous Quotes</h1>
+      <ul>
+        {quotes.map((quote) => (
+          <li>
+            <h2>{quote.quote}</h2>
+            <h3>{quote.author}</h3>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
